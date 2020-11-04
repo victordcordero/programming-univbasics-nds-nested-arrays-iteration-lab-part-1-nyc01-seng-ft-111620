e4831534fc4ab428d76cfa5a373ff82661bf4101
Victor Cordero <victordcordero@gmail.com>
2020-11-04 22:27:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,8 +14,8 @@ while count < array_of_arrays.length do
   p array_of_arrays[count]
 
   inner_count = 0
-  while inner_count < array_of_arrays[count].length do
-    p array_of_arrays.even? 
+  while inner_count < array_of_arrays[count].even? do
+    p array_of_arrays[count][inner_count]
     inner_count += 1
   end
 
