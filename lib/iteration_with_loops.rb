d870e61b7389c24a2a8e78fd853ff85ab70fd535
Victor Cordero <victordcordero@gmail.com>
2020-11-04 22:24:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,5 +8,18 @@ array_of_arrays = [
  [23, 41]
 ]
 
+count = 0
+
+while count < array_of_arrays.length do
+  p array_of_arrays[count]
+
+  inner_count = 0
+  while inner_count < array_of_arrays[count].length do
+    p array_of_arrays[count][inner_count]
+    inner_count += 1
+  end
+
+  count += 1
+end
 
 end
